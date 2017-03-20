64ffe744c495f45ee88972e7b11f7103922aa80c
INGJoseRamos <ing.ramosgarces@gmail.com>
2017-03-20 01:00:57 -0500

    Update go.php

--- go.php
+++ go.php
@@ -5,8 +5,12 @@
 	mysql_connect("127.0.0.1", "root") or die(mysql_error());
 	mysql_select_db("dato") or die(mysql_error());
 
+	$consulta = "SELECT clicks from votaciones WHERE id='$id'";
+	$votos=mysql_query($consulta) or die (mysql_error());
+	$votos=$votos+1;
+
 	// Incrementamos en 1 el contador del link con la ID especificada en la url
-	$update = "UPDATE votaciones SET clicks=(clicks + 1) WHERE id='$id'";
+	$update = "UPDATE votaciones SET clicks='$votos' WHERE id='$id'";
 	mysql_query($update) or die (mysql_error());
 
-?>	
+?>		
